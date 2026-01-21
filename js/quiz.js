9c17b156c634883d7a493ba83748fdc81ec2983a
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-01-21 15:20:07 +0100

    new tests

--- js/quiz.js
+++ js/quiz.js
@@ -172,9 +172,9 @@
 //! function getExtremeElements(array) {
 //!   const lastIndex = array.length - 1;
 //!   const lastElement = array[lastIndex];
-!
+
 //!   const firstElement = array[0];
-!
+
 //!   const newArray = [firstElement, lastElement];
 //!   return newArray;
 //! }
@@ -450,3 +450,133 @@
 //? []
 
 // ------------------------------------------------------------------
+
+// Виконай рефакторинг функції countProps(object), замінивши
+// перебір ключів за допомогою циклу for…in на метод Object.keys()
+//  для отримання масиву властивостей. Функція має повернути
+//  кількість властивостей в об'єкті object.
+
+//! function countProps(object) {
+//!   const keys = Object.keys(object);
+//!   return keys.length;
+//! }
+
+//? console.log(countProps({}));
+//? // 0
+//? console.log(countProps({ name: "Mango", age: 2 }));
+//? // 2
+//? console.log(countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }));
+//? // 3
+
+// ------------------------------------------------------------------
+
+// Масив colors містить колекцію кольорів. Кожен колір
+//  представлений об'єктом і має властивості hex і rgb
+//  з відповідними для цього формату і кольору
+//  значеннями.
+
+// Перебери масив об'єктів colors, використовуючи цикл
+// for...of. Додай у масив hexColors значення
+// властивостей hex, а в масив rgbColors - значення
+//  властивостей rgb з усіх об'єктів масиву colors.
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+
+// console.log(hexColors);
+// // ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// console.log(rgbColors);
+// // ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+
+// ------------------------------------------------------------------
+
+// Напиши функцію getAllPropValues(propName), яка приймає один
+//  параметр propName - ім'я (ключ) властивості. Функція повинна
+//  повернути масив усіх значень властивості з таким ім'ям з
+// кожного об'єкта в масиві products. Якщо в об'єктах відсутні
+//  властивості з таким ім'ям, функція повинна повернути порожній
+//  масив.
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   const array = [];
+
+//   for (const product of products) {
+//     if (propName in product) {
+//       array.push(product[propName]);
+//     }
+//   }
+
+//   return array;
+
+//   return [];
+// }
+
+// console.log(getAllPropValues("name"));
+// // ["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues("quantity"));
+// // [4, 3, 7, 9];
+// console.log(getAllPropValues("price"));
+// // [1300, 2700, 400, 1200]
+// console.log(getAllPropValues("category"));
+// // [];
+
+// ------------------------------------------------------------------
+
+// Функція calculateTotalPrice(productName) приймає один параметр
+//  productName- назва товару. Функція містить масив об'єктів
+//  products з такими властивостями, як name — ім'я товару,
+//  price — ціна і quantity — кількість.
+
+// Доповни код функції так, щоб вона повертала загальну вартість
+//  (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна
+//  повертати рядок "Product <productName> not found!" , де
+//  <productName> — це ім'я товару.
+
+function calculateTotalPrice(productName) {
+  const products = [
+    { name: "Radar", price: 1300, quantity: 4 },
+    { name: "Scanner", price: 2700, quantity: 3 },
+    { name: "Droid", price: 400, quantity: 7 },
+    { name: "Grip", price: 1200, quantity: 9 },
+  ];
+
+  for (const product of products) {
+    if (product.name === productName) {
+      return product.price * product.quantity;
+    }
+  }
+
+  return `Product ${productName} not found!`;
+}
+
+console.log(calculateTotalPrice("Blaster"));
+// "Product Blaster not found!";
+console.log(calculateTotalPrice("Radar"));
+// 5200;
+console.log(calculateTotalPrice("Droid"));
+// 2800;
+console.log(calculateTotalPrice("Grip"));
+// 10800;
+console.log(calculateTotalPrice("Scanner"));
+// 8100;
