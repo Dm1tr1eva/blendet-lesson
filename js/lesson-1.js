6b8327a815d0ca99a57319bf973d4c4e4baac711
Dm1tr1eva <114660725+Dm1tr1eva@users.noreply.github.com>
2026-03-05 19:17:45 +0100

    done lesson 1

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -1,97 +1,177 @@
-// У змінній min лежить випадкове число від 0 до 59.
-// Визначте, в яку чверть години потрапляє
-// це число (у першу, другу, третю чи четверту).
-// Виведіть в alert відповідне повідомлення, наприклад:
-// "10 входить в першу чверть"
+// !Завдання 1:
+
+// function getPrompt() {
+//   const number = Number(prompt("Введіть число:", ""));
+
+//   if (number === 10) {
+//     alert("Вірно");
+//   } else {
+//     alert("Невірно");
+//   }
+// }
+
+// getPrompt();
+
+// !Завдання 2:
 
 // const min = Math.floor(Math.random() * 59);
-// Math.floor(Math.random() * (max - min + 1)) + min;
 
-// const min = Math.floor(Math.random() * (59 - 0 + 1)) + 0;
-
-// function rand(min) {
-
-//     if (min <= 15) {
-//       alert(`${min} входить в першу чверть`);
-//     } else if (min <= 30) {
-//       alert(`${min} входить в другу чверть`);
-//     } else if (min <= 45) {
-//     alert (`${min} входить в третю чверть`);
-//     } else if (min <= 59){
-//       alert(`${min} входить в четверту чверть`);
-//     }
+// if (min >= 0 && min <= 15) {
+//   alert(`${min} входить в першу чверть`);
+// } else if (min >= 16 && min <= 30) {
+//   alert(`${min} входить в другу чверть`);
+// } else if (min >= 31 && min <= 45) {
+//   alert(`${min} входить в третю чверть`);
+// } else {
+//   alert(`${min} входить в четверту чверть`);
 // }
 
-// rand(min)
+// !Завдання 3:
 
-// ---------------------------------------------------------------------------------
+// const num = Number(prompt("Введіть число від 1 до 4"));
+// let result = "";
 
-// Змінна num може набувати 4 значення: '1', '2', '3' або '4'
-// (запитуй це значення у користувача через prompt).
-// Якщо вона має значення '1', то у змінну result запишіть 'зима',
-// якщо має значення '2' - 'весна' і так далі.
-// Розв'яжіть завдання через switch-case.
-// Не забудьте про дефолтне значення, на випадок, якщо користувач
-// введе в prompt щось інше. В такому випадку result має набувати значення:
-// "Вибачте, але ви маєте ввести значення від 1 до 4 включно".
-// Значення змінної result виведіть в консоль.
+// switch (num) {
+//   case 1:
+//     result = "Зима";
 
-// const num = prompt("число від 1 до 4");
+//     break;
+//   case 2:
+//     result = "Весна";
 
-// function season(num) {
-//   let result;
+//     break;
+//   case 3:
+//     result = "Літо";
 
-//   switch (num) {
-//     case "1":
-//       result = "зима";
-//       break;
-//     case "2":
-//       result = "весна";
-//       break;
-//     case "3":
-//       result = "літо";
-//       break;
-//     case "4":
-//       result = "осінь";
-//       break;
-//     default:
-//       result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
-//     }
+//     break;
+//   case 4:
+//     result = "Осінь";
 
-//     return result;
+//     break;
+//   default:
+//     result = "Вибачте, але ви маєте ввести значення від 1 до 4 включно";
 // }
 
-// console.log(season(num));
+// console.log(result);
 
-// ------------------------------------------------------------------------------------------
+// !Завдання 4:
 
-// Отримуйте від користувача число (кількість хвилин) через prompt
-// і виводьте у консоль рядок у форматі годин та хвилин.
-// Приклад: користувач вводить в prompt '70' -> в консолі відобразиться '01:10'.
-// Корисне посилання для відображення годин та хвилин у потрібному форматі ('01' замість '1'):
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart#examples
+// const userMinutes = Number(prompt("Введіть кількість хвилин:"));
 
-// const num = Number(prompt("введіть число"));
-
-// function whatTime(num) {
-//   // Перевірка некоректного вводу
-//   if (!Number.isFinite(num) || num < 0 || !Number.isInteger(num)) {
-//     return "Некоректне значення";
-//   }
-//   // Обмеження до 24 годин
-//   const totalMinutes = num % 1440;
-
+// function getTime(totalMinutes) {
 //   const hours = Math.floor(totalMinutes / 60);
 //   const minutes = totalMinutes % 60;
 
-//   const stringHours = String(hours).padStart(2, "0");
-//   const stringMinutes = String(minutes).padStart(2, "0");
+//   const formattedHours = String(hours).padStart(2, "0");
+//   const formattedMinutes = String(minutes).padStart(2, "0");
 
-//   return `${stringHours}:${stringMinutes}`;
+//   return `${formattedHours}:${formattedMinutes}`;
+// }
+// console.log(getTime(userMinutes));
+
+// !Завдання 4.1:
+
+// const userMinutes = Number(prompt("Введіть кількість секунд:"));
+
+// function getTime(fullTime) {
+//   const hour = Math.floor(fullTime / 3600);
+//   const minutes = Math.floor((fullTime % 3600) / 60);
+//   const seconds = fullTime % 60;
+
+//   const formattedHours = String(hour).padStart(2, "0");
+//   const formattedMinutes = String(minutes).padStart(2, "0");
+//   const formattedSeconds = String(seconds).padStart(2, "0");
+
+//   return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
 // }
 
-// console.log(whatTime(num));
+// console.log(getTime(userMinutes));
 
+// !Завдання 5:
 
+// const login = prompt("Введіть логін:");
 
+// console.log(login);
 
+// if (login === "Адмін") {
+//     const password = prompt("Введіть пароль:");
+//     if (password === "Я головний") {
+//     alert("Добрий день!");
+//     } else {
+//     alert("Невірний пароль!");
+//     }
+// } else if (login === "" || login === null) {
+//   alert("Скасовано");
+// } else {
+//   alert("Я вас не знаю");
+// }
+
+// !Завдання 6:
+
+// let num = 0;
+
+// while (num <= 20) {
+//   console.log(num++);
+// }
+
+// !Завдання 7:
+
+// function getNumbers(min, max) {
+//   let sum = 0;
+
+//   for (let i = min; i <= max; i++) {
+//     console.log(i);
+
+//     if (i % 2 === 0) {
+//       sum += i;
+//     }
+//   }
+
+//   return sum;
+// }
+
+// console.log(getNumbers(1, 10));
+
+// !Завдання 8:
+
+// function min(a, b) {
+//   if (Number.isFinite(a) && Number.isFinite(b)) {
+//     if (a < b) {
+//       return a;
+//     } else {
+//       return b;
+//     }
+//   } else {
+//     return "Not a number!";
+//   }
+// }
+
+// console.log(min(20, 3));
+
+// !Завдання 9:
+
+// function isAdult(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return confirm("Вам є 18 років?");
+//   }
+// }
+
+// console.log(isAdult(2));
+
+// !Завдання 10:
+
+// function fizzBuzz(num) {
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("fizzbuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("buzz");
+//     }
+//     }
+// }
+
+// fizzBuzz(12);
